a8e13fe25a5d0a03b78ebe2fc194950b626e77bb
Jake Yeo <jyeo03@students.cs.ubc.ca>
2024-08-31 13:37:29 -0700

    Made responsive header

--- src/components/SharedComponents/Header.tsx
+++ src/components/SharedComponents/Header.tsx
@@ -1,25 +1,77 @@
-import { Box, Button, Stack } from "@mui/material";
+import { Box, Button, Drawer, List, ListItem, ListItemButton, Stack, Typography } from "@mui/material";
 import Logo from "./Logo";
 import CustomButton from "./CustomButton";
+import React, { useEffect, useState } from "react";
+import FontStyles from "./FontStyles";
 
 interface props {
     aboutRef: React.RefObject<HTMLDivElement>,
     skillsRef: React.RefObject<HTMLDivElement>,
     projectsRef: React.RefObject<HTMLDivElement>,
-    contactRef: React.RefObject<HTMLDivElement>
+    contactRef: React.RefObject<HTMLDivElement>,
+    scrollBoxRef: React.RefObject<HTMLDivElement>,
 }
 
-const Header = ({ aboutRef, skillsRef, projectsRef, contactRef }: props) => {
+const Header = ({ aboutRef, skillsRef, projectsRef, contactRef, scrollBoxRef }: props) => {
 
-    // 427px is the minimum width before the buttons look weird
+    // 430px is the minimum width before the buttons look weird
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        // Add event listener to update width on window resize
+        window.addEventListener('resize', handleResize);
+
+        // Clean up the event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const [open, setOpen] = React.useState(false);
+
+    const toggleDrawer = (newOpen: boolean) => () => {
+        setOpen(newOpen);
+    };
+
+    function getDrawerItem(text: string, onClick: () => void) {
+        return (
+            <ListItem disablePadding>
+                <ListItemButton onClick={onClick}>
+                    <FontStyles></FontStyles>
+                    <Typography textAlign={"center"} fontWeight={200} fontSize={"10px"} fontFamily={"'Poppins', sans-serif"} color={"white"}>
+                        {text}
+                    </Typography>
+                </ListItemButton>
+            </ListItem>)
+    }
+
+    const DrawerList =
+        <>
+            <Box sx={{ width: 100 }} role="presentation" onClick={toggleDrawer(false)}>
+                <List>
+                    {getDrawerItem("About Me", () => { scrollTo(aboutRef) })}
+                    {getDrawerItem("My Skills", () => { scrollTo(skillsRef) })}
+                    {getDrawerItem("My Projects", () => { scrollTo(projectsRef) })}
+                    {getDrawerItem("Contact", () => { scrollTo(contactRef) })}
+                </List>
+            </Box>
+        </>
 
     function scrollTo(ref: React.RefObject<HTMLDivElement>) {
-        if (ref.current) {
-            const elementPosition = ref.current.getBoundingClientRect().top + window.scrollY;
-            const offsetPosition = elementPosition - 40; // Adjust for 20px from the top
+        if (ref.current && scrollBoxRef.current) {
+            const boxTop = scrollBoxRef.current.getBoundingClientRect().top;
+            const elementTop = ref.current.getBoundingClientRect().top;
 
-            window.scrollTo({
-                top: offsetPosition,
+            // Calculate the element's position relative to the scrollBoxRef
+            const offsetPosition = elementTop - boxTop - 40; // Adjust for offset (40px)
+
+            scrollBoxRef.current.scrollTo({
+                top: scrollBoxRef.current.scrollTop + offsetPosition,
                 behavior: 'smooth',
             });
         }
@@ -62,7 +114,19 @@ const Header = ({ aboutRef, skillsRef, projectsRef, contactRef }: props) => {
                 left: 0,
             }}>
             <Logo padding={"10px"} height={30}></Logo>
-            {windowsFormat}
+            <Drawer anchor="right" sx={{
+                "& .MuiPaper-root": {
+                    backgroundColor: "#686868", // Set the background color of the Drawer
+                }
+            }} open={open} onClose={toggleDrawer(false)}>
+                {DrawerList}
+            </Drawer>
+            {windowWidth < 430
+                ?
+                <CustomButton onClick={toggleDrawer(true)} text={"Menu"} fontSize="10px" minWidth="0px" width="52.88px" height="20px" boxShadow="none" backgroundColor="black" marginRight="10px"></CustomButton>
+                :
+                windowsFormat
+            }
         </Stack >
     </>);
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 
-import { Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import Logo from "../components/SharedComponents/Logo";
 import Header from "../components/SharedComponents/Header";
 import AboutMe from "./AboutMe";
@@ -15,17 +15,20 @@ const HomePage = () => {
     const skillsRef = useRef(null);
     const projectsRef = useRef(null);
     const contactRef = useRef(null);
+    const scrollBoxRef = useRef(null);
 
     return (<>
-        <Stack justifyContent={"center"} alignItems={"center"} width={"100%"}>
-            <Header aboutRef={aboutRef} skillsRef={skillsRef} projectsRef={projectsRef} contactRef={contactRef}></Header>
-            <AboutMe ref={aboutRef}></AboutMe>
-            <MySkills ref={skillsRef}></MySkills>
-            <MyProjects ref={projectsRef}></MyProjects>
-            <Contact ref={contactRef}></Contact>
-            <FontStyles />
-            <Typography width="100%" textAlign={"center"} fontSize={"8px"} fontWeight={100} fontFamily={"'Poppins', sans-serif"} color={"white"}>Copyright 2024 © Jake Yeo</Typography>
-        </Stack>
+        <Box ref={scrollBoxRef} sx={{ maxHeight: "100vh", overflowY: "scroll" }}> {/**Make box the scrollable container so that drawer does not get rid of scroll bar when  */}
+            <Header aboutRef={aboutRef} skillsRef={skillsRef} projectsRef={projectsRef} contactRef={contactRef} scrollBoxRef={scrollBoxRef}></Header>
+            <Stack justifyContent={"center"} alignItems={"center"} width={"100%"}>
+                <AboutMe ref={aboutRef}></AboutMe>
+                <MySkills ref={skillsRef}></MySkills>
+                <MyProjects ref={projectsRef}></MyProjects>
+                <Contact ref={contactRef}></Contact>
+                <FontStyles />
+                <Typography width="100%" textAlign={"center"} fontSize={"8px"} fontWeight={100} fontFamily={"'Poppins', sans-serif"} color={"white"}>Copyright 2024 © Jake Yeo</Typography>
+            </Stack>
+        </Box>
     </>)
 }
 
